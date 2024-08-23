decc008398dccd50df00f83bb95d1eed5026c7e8
Arrascaeta Nahuel <nahuelarrascaeta22@gmail.com>
2024-08-23 19:11:07 -0300

    Change apiUrl

--- src/app/layout/urlservice.ts
+++ src/app/layout/urlservice.ts
@@ -1,3 +1,3 @@
 const localApi = "http://localhost:5117/api/";
-const prodApi = "https://01b6-190-7-7-138.ngrok-free.app/api/";
+const prodApi = "https://b54d-190-7-7-138.ngrok-free.app/api/";
 export const apiUrl = prodApi;
