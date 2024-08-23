41ee0c039bf05a8fe7b5cd6377f898c5607963b9
Arrascaeta Nahuel <nahuelarrascaeta22@gmail.com>
2024-08-23 18:48:19 -0300

    Add prod url api

--- src/app/layout/urlservice.ts
+++ src/app/layout/urlservice.ts
@@ -1,3 +1,3 @@
 const localApi = "http://localhost:5117/api/";
-const prodApi = "https://tim-vehiculos.store/casajulianservice/v1/api/";
+const prodApi = "https://01b6-190-7-7-138.ngrok-free.app/api/";
 export const apiUrl = prodApi;
